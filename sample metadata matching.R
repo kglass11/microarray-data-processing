e362fda1e8471a47c8d3265d087f648826e7bcca
Katherine Glass <katherine.glass@lshtm.ac.uk>
2018-02-07 17:01:50 +0000

    another script for matching sample and metadata
    I just started this.

---
+++ sample metadata matching.R
@@ -0,0 +1,18 @@
+### Function for matching samples out of order with metadata from another file. 
+
+# file1 is the sample list file (from your assay), file2 is the file name from your sample metadata
+# input both as character vectors
+
+# If the list for file1 is incomplete or out of order, we need to be able to match the sample metadata
+# only for the samples we have run on the slides.
+
+sample_list <- ""
+metadata <- ""
+
+function (file1, file2){
+  
+  #read in both files as data frames
+  read.csv()
+  
+  merge()
+}
