ba7afe20f0275f4305ffdefc5d95abc88b7d3c69
kglass11 <katherine.glass@lshtm.ac.uk>
2018-04-13 19:56:37 +0100

    extra script to make single antigen single dilution plots

---
+++ OptimizationAntigenPlots.R
@@ -0,0 +1,32 @@
+#Optimization single antigen, single dilution plots 
+
+antigen = "GexP18"
+dilution = "100"
+colnames(AHHHH.df[29])
+
+ #all samples and all variables - Scatter plot faceted
+ png(filename = paste0(antigen,"_", dilution, "_facet_grid.tif"), width = 8, height = 5, units = "in", res = 1200)
+ par(mfrow=c(1,1), oma=c(3,1,1,1),mar=c(4.1,4.1,3.1,2.1))
+
+ggplot(AHHHH.df, aes(x=slide_type, y=X31_1.GEXP18.100ug.ml_1, color = sample, shape = blocking_buffer)) +
+   geom_point(size = 1)  +
+   theme_bw() +
+   theme(text = element_text(size=9), axis.text.x = element_text(angle = 90, hjust=0.95, vjust = 0.1)) +
+   labs(x = "Slide Type", y = "Normalized Log2(Positive/Negative)", title = paste(antigen, dilution, "µg/mL")) +
+   facet_grid(print_buffer ~ block_dilution )
+
+ graphics.off()
+
+#violin plot of slide type and print buffer, everything else combined
+ png(filename = paste0(antigen,"_", dilution, "_ST_PB.tif"), width = 8, height = 5, units = "in", res = 1200)
+ par(mfrow=c(1,1), oma=c(3,1,1,1),mar=c(4.1,4.1,3.1,2.1))
+
+ggplot(AHHHH.df, aes(x=slide_type, y=X31_1.GEXP18.100ug.ml_1, color = print_buffer, fill = print_buffer)) +
+   geom_violin(trim = FALSE) +
+   theme_bw() +
+   guides(color=FALSE) +
+   theme(text = element_text(size=9), axis.text.x = element_text(angle = 45, hjust = 1), legend.position="top") +
+   labs(x = "Slide Type", y = "Normalized Log2(Positive/Negative)", title = paste(antigen, dilution, "µg/mL"), fill = "Print Buffer")
+
+graphics.off()
+
